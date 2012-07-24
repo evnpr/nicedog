b53b6d6bbf2ccb31f5d9675618ffe2dd6b577e99
evanpy <evanpurnama5@gmail.com>
2012-07-24 10:42:27 +0800

    commit

---
+++ views/index.php
@@ -0,0 +1,6 @@
+<div id="name" class="hero-unit">
+   <h1>Welcome to HomePage</h1><br>
+   <button class="btn-large btn-primary">read more</button>
+</div>
+
+<p><?=$halo?></p>
---
+++ views/tag.php
@@ -0,0 +1,9 @@
+
+
+<?=$halo?>
+<form action="<?=site_url()?>/post" method="post" accept-charset="utf-8">
+    
+<input type="text" name="name" value="">
+
+<p><input type="submit" value="SUBMIT" class="btn-primary"></p>
+</form>
